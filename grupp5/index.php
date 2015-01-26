1870b6d86b9c541a180fdd00656bab5613bb9f11
marben431 <marben431@E29772.edu.linkoping.se>
2015-01-26 10:55:51 +0100

    #3

--- grupp5/index.php
+++ grupp5/index.php
@@ -14,11 +14,11 @@
         $dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
 
 
-        $namn = filter_input(INPUT_POST, 'namn', FILTER_SANITIZE_SPECIAL_CHARS);
-        $enamn = filter_input(INPUT_POST, 'enamn', FILTER_SANITIZE_SPECIAL_CHARS);
-        $usr = filter_input(INPUT_POST, 'usr', FILTER_SANITIZE_SPECIAL_CHARS);
-        $klass = filter_input(INPUT_POST, 'klass', FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+//        $namn = filter_input(INPUT_POST, 'namn', FILTER_SANITIZE_SPECIAL_CHARS);
+//        $enamn = filter_input(INPUT_POST, 'enamn', FILTER_SANITIZE_SPECIAL_CHARS);
+//        $usr = filter_input(INPUT_POST, 'usr', FILTER_SANITIZE_SPECIAL_CHARS);
+//        $klass = filter_input(INPUT_POST, 'klass', FILTER_SANITIZE_SPECIAL_CHARS);
+//        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
 
         $file = fopen("ant/phpcsvfil.csv", "r");
 
@@ -26,11 +26,15 @@
             //print_r(fgetcsv($file, 1000, ";"));
             $arr = fgetcsv($file, 1000, ";");
             //var_dump($arr);
-            echo "<tt>namn    : " . $arr[0] . "<br>";
-            echo "   enamn: " . $arr[1] . "<br>";
+            echo "<br>";
+            echo "<tt>namn   : " . $arr[0] . "<br>";
+            echo "     enamn: " . $arr[1] . "<br>";
             echo "     usr: " . $arr[2] . "<br>";
             echo "   klass: " . $arr[3] . "<br><br></tt>";
-            $sql = "INSERT INTO `inlog`(`id`, `filnamn`, `namn`, `efternamn`, ``, `mail`) VALUES ('','" . $filnamn . "','" . $namn . "','" . $efternamn . "','" . $telefonnummer . "','" . $mail . "')";
+            
+            
+            $sql = "INSERT INTO `testsak`(`id`,`namn`, `enamn`, `usr`, `klass`) VALUES ('','" . $arr[0] . "','" . $arr[1] . "','" . $arr[2] . "','" . $arr[3] . "')";
+            echo $sql;
             $stmt = $dbh->prepare($sql);
             $stmt->execute();
         }
